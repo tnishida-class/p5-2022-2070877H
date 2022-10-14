62baa5a53e75b59d9885da6c420773e6acce38c4
2070877H <114969370+2070877H@users.noreply.github.com>
2022-10-14 23:17:53 +0900

    Change section1-1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,5 +2,16 @@
 function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+   if(i<5) {
+    stroke(255,0,0);
+    let x=(100-10*i);
+   ellipse(50,50,x);
+   }
+   else{
+    stroke(0,0,255);
+    let x=(100-10*i);
+   ellipse(50,50,x);
+   }
+   
   }
 }
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,43 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+      console.log(i, j); // 補足： i,j の変化がわかりやすくなるように入れています
+      strokeWeight(0);
+      let n=i+j;
+      if(n%2 == 1)
+        {
+            //jが0から2の時は赤丸
+          if(j<=2)
+          {
+
+          fill(255,0,0);//赤色
+          ellipse(size*i+size*0.5,size*j+size*0.5,25);
+
+          fill(211);//色を灰色
+          rect(i * size, j * size, size,size);
+
+      
+          }
+            //jが3と4の時
+          if(j==3&&j==4)
+          {
+
+          }
+            //jが5から7の時は黒丸
+          if(5<=j<=7)
+          {
+
+          }
+
+          fill(211);//色を灰色
+          rect(i * 25, j * 25, 25,25);
+        }
+      
+      else{
+        fill(255);//色を白色に
+        rect(i * 25, j * 25, 25,25);
+      }
   }
-}
+  }
+  }
+
