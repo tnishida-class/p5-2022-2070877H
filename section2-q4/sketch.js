474f3e25d64bfa128566b3265f873b317a85b20f
2070877H <114969370+2070877H@users.noreply.github.com>
2022-11-15 14:39:57 +0900

    homework2-q4 2070877H

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -8,14 +8,24 @@ function setup() {
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
+    if(i%2==0){
+      fill(blue);
+      rect(0,i*d,width,(i+1)*d);
+    }
+    else{
+      fill(255,255,255);
+      rect(0,i*d,width,(i+1)*d);
+    }
   }
 
   fill(blue);
+  stroke(blue);
   let size = d * 5;
   rect(0, 0, size, size);
 
   fill(255);
+  stroke(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0,d*2,size,d);
+  rect(0.5*size,0,d,size);
 }
