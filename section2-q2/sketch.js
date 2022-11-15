2481b68d4ad7ba4043e26054b10e62418a5d3620
2070877H <114969370+2070877H@users.noreply.github.com>
2022-11-15 11:35:54 +0900

    homework2-q2　2070877H

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,8 +3,8 @@ function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
+  for(let i = 0; i < 9; i++){
+    for(let j = 0; j < 9; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
       console.log(i, j); // 補足： i,j の変化がわかりやすくなるように入れています
       strokeWeight(0);
@@ -15,16 +15,12 @@ function setup() {
           if(j<=3)
           {
 
- 
-          let u=(i+1)*25;
-          let I=u-12.5;
+          let u=(i-0.5)*25;
+          let I=u+25;
           let J=j*25;
             
-          fill(255,0,0);//赤色
+          fill(255,0,0);//赤
           ellipse(I-25,J-12.5,25); 
-
-          fill(255,0,0);
-          ellipse(187.5,12.5,25);
 
             
       
@@ -36,7 +32,7 @@ function setup() {
 
           }
             //jが5から7の時は黒丸
-          if(6<=j&&j<=8)
+          if(6<=j&&j<=9)
           {
          // let a=i*25
           //let A=a+12.5
@@ -47,19 +43,17 @@ function setup() {
 
 
 
-          let u=(i+1)*25;
-          let I=u-12.5;
-          let j=j*25;
+          let u=(i+0.5)*25;
+          let I=u;
+          let J=j*25;
             
-          fill(0,0,0);//赤色
-          ellipse(I+12.5,J,25); 
+          fill(0,0,0);//黒色
+          ellipse(I-25,J-12.5,25); 
           }
 
           fill(211);//色を灰色
           rect(i * 25, j * 25, 25,25);
 
-          fill(255,0,0);
-          ellipse(187.5,62.5,25);
         }
       
       else{
